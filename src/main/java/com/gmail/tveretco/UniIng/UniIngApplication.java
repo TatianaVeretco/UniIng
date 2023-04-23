18f015faebacd046902ec091a3b30d0b1a597f7b
TatianaVeretco <tveretco@gmail.com>
2023-04-23 14:57:04 +0300

    Inet project

---
+++ src/main/java/com/gmail/tveretco/UniIng/UniIngApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.tveretco.UniIng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniIngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniIngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/tveretco/UniIng/UniIngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.tveretco.UniIng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniIngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
